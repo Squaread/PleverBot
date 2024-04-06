18991a93661322b260fb4de356466b1e026d803f
Sys32_4b7P <134743338+Squaread@users.noreply.github.com>
2024-04-06 17:04:45 -0300

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,7 @@ bot_prefix = ';' # Bot prefix
 TOKEN = "TOKEN" # Bot token
 
 # Tenha certeza que o bot tem acesso ao canal, se não vai resultar em erros | Make sure the bot has access to the channel, otherwise errors will result
-bot_log_channel = 0 # É obrigatorio um ID valido | A valid ID is required
+bot_log_channel = 0 # Deixe 0 para desativar as logs (não recomendado) | Enter 0 to disable logs (not recommended)
+
+# Intervalo do envio entre as garrafas (minutos) | Shipping interval between bottles (minutes)
+bottle_interval = 20 
